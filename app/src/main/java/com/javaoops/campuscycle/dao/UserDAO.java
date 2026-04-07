139eb673918d1919422a668e7d55d2406814babb
HetanshiVora <hetanshi531@gmail.com>
2026-04-07 17:25:33 +0530

    UserDAO created

---
+++ app/src/main/java/com/javaoops/campuscycle/dao/UserDAO.java
@@ -0,0 +1,53 @@
+package com.javaoops.campuscycle.dao;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.javaoops.campuscycle.model.Seller;
+import com.javaoops.campuscycle.model.User;
+
+public class UserDAO {
+    private DatabaseHelper dbHelper;
+
+    public UserDAO(Context context) {
+        dbHelper = new DatabaseHelper(context);
+    }
+
+    public boolean insertUser(User user) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("userId", user.getUserId());
+        values.put("name", user.getName());
+        values.put("universityId", user.getUniversityId());
+        values.put("email", user.getEmail());
+        values.put("role", user.getRole());
+        values.put("isVerified", user.isVerified() ? 1 : 0);
+        long result = db.insert("users", null, values);
+        db.close();
+        return result != -1;
+    }
+
+    public User getUserById(String userId) {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        User user = null;
+        Cursor cursor = db.query("users", null, "userId=?", new String[]{userId}, null, null, null);
+        if (cursor.moveToFirst()) {
+            String name = cursor.getString(cursor.getColumnIndexOrThrow("name"));
+            String universityId = cursor.getString(cursor.getColumnIndexOrThrow("universityId"));
+            String email = cursor.getString(cursor.getColumnIndexOrThrow("email"));
+            String role = cursor.getString(cursor.getColumnIndexOrThrow("role"));
+            int isVerified = cursor.getInt(cursor.getColumnIndexOrThrow("isVerified"));
+
+            user = new Seller(userId, name, universityId, email);
+            user.setRole(role);
+            user.setVerified(isVerified == 1);
+        }
+        cursor.close();
+        db.close();
+        return user;
+    }
+
+
+}
