f7b1763bfc85eb6d756179ccd85acfaaacc858e7
HetanshiVora <hetanshi531@gmail.com>
2026-04-07 16:22:55 +0530

    Created and added DatabaseHelper file

---
+++ app/src/main/java/com/javaoops/campuscycle/dao/DatabaseHelper.java
@@ -0,0 +1,32 @@
+package com.javaoops.campuscycle.dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "campuscycle.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE users (userId TEXT PRIMARY KEY, name TEXT, universityId TEXT, email TEXT, role TEXT, isVerified INTEGER)");
+        db.execSQL("CREATE TABLE products (productId TEXT PRIMARY KEY, title TEXT, description TEXT, category TEXT," +
+                "mrp REAL, price REAL, sellerId TEXT, status TEXT, viewCount INTEGER, cartCount INTEGER, timestamp INTEGER)");
+        db.execSQL("CREATE TABLE orders (orderId TEXT PRIMARY KEY, productId TEXT, buyerId TEXT, sellerId TEXT, amountPaid REAL, orderStatus TEXT, timestamp INTEGER)");
+        db.execSQL("CREATE TABLE notifications(notifId TEXT PRIMARY KEY, targetUserId TEXT, productId TEXT, type TEXT, message TEXT, isRead INTEGER, timestamp INTEGER)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS users");
+        db.execSQL("DROP TABLE IF EXISTS products");
+        db.execSQL("DROP TABLE IF EXISTS orders");
+        db.execSQL("DROP TABLE IF EXISTS notifications");
+        onCreate(db);
+    }
+}
