da8303f7185e5349d7975ea5dfe7793afad1ab5c
Anshika Gupta <anshika.akg19@gmail.com>
2026-04-08 08:31:49 +0530

    Increment database version to 2

--- app/src/main/java/com/javaoops/campuscycle/dao/DatabaseHelper.java
+++ app/src/main/java/com/javaoops/campuscycle/dao/DatabaseHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "campuscycle.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
